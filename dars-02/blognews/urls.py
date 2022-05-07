bf1ec697e490f7aa8b35547519d387305bdb2c39
Rustam <rustamisomiddinovh77@gmail.com>
2022-05-07 16:15:14 +0500

    yangi dars
    create, update va delete tugmalarni qoshishn o'rgandim.

--- dars-02/blognews/urls.py
+++ dars-02/blognews/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import HomePageView, BlogDetailView
+from .views import (
+    HomePageView, 
+    BlogDetailView, 
+    BlogCreateView, 
+    BlogEditView, 
+    BlogDeleteView
+    )
 
 urlpatterns = [
     path('',HomePageView.as_view(),name='home'),
-    path('post/<int:pk>/',BlogDetailView.as_view(),name='post_detail')
+    path('post/<int:pk>/',BlogDetailView.as_view(),name='post_detail'),
+    path('post/create/',BlogCreateView.as_view(),name='post_create'),
+    path('post/<int:pk>/edit/',BlogEditView.as_view(),name='post_edit'),
+    path('post/<int:pk>/delete/',BlogDeleteView.as_view(),name='post_delete'),
 ]
